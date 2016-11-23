ea212ba0767b8668e34156e1aeedae588e51b195
Juergen Fiedler <juergen@fiedlerfamily.net>
2016-11-23 00:59:26 -0500

    Improve the logic to detect existing partitions.

--- config/includes.chroot/root/bin/rebuild_fstab.sh
+++ config/includes.chroot/root/bin/rebuild_fstab.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-DRIVES=$(fdisk -l | grep -v swap |grep -o '^/dev/sd[a-z][1-9]')
+DRIVES=$(fdisk -l | grep -v swap | grep -v Extended | grep -o '^/dev/sd[a-z][1-9]')
 
 cp /etc/fstab /etc/fstab.old
 grep -v " auto " /etc/fstab.old > /etc/fstab
@@ -9,7 +9,7 @@ for DRIVE in $DRIVES
 do
   MOUNT=$(echo $DRIVE|sed 's/dev/mnt/')
   mkdir -p $MOUNT
-  echo $DRIVE $MOUNT auto noauto 0 0 >> /etc/fstab
+  echo $DRIVE $MOUNT auto 0 0 >> /etc/fstab
 done
 
 DIFF=$(diff /etc/fstab.old /etc/fstab | grep '<')
