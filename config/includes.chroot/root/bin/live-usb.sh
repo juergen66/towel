525c3f5a6e5117d96b8c5c7bcab14c4c6f5554ed
Juergen Fiedler <juergen@fiedlerfamily.net>
2018-12-06 22:09:01 -0500

    Replace /root/bin/live-usb w. simple live-usb.sh.

---
+++ config/includes.chroot/root/bin/live-usb.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+echo ''
+echo '***************************************************'
+echo '*** FOR THE SAKE OF ALL THAT IS GOOD AND SANE,  ***'
+echo '*** PLEASE EDIT ME CAREFULLY BEFORE RUNNING ME! ***'
+echo '***************************************************'
+echo ''
+exit 42
+
+# This should be the only variable that needs to be edited
+# Everything on this drive will be utterly annihilated!!!!
+DRIVE=/dev/sdX
+
+set -x
+
+source="/run/live/medium/"
+target=$(mktemp -d /tmp/live-usb.XXXXX)/
+part=${DRIVE}1
+
+parted -s ${DRIVE} mktable msdos
+parted -s ${DRIVE} mkpart primary fat32 0% 100%
+parted -s ${DRIVE} set 1 boot on
+
+sync
+
+mkfs.vfat ${part}
+
+sync
+
+mount ${part} ${target}
+
+rsync -a ${source}  ${target}
+
+rm -rf ${target}/isolinux/
+
+grub-install --root-directory=${target} ${DRIVE}
+
+umount ${target}
+rmdir  ${target}
