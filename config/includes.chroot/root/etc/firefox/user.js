7de947f5f906ec246fca0a1daf5257f3ebc6fc44
Juergen Fiedler <juergen@fiedlerfamily.net>
2022-02-23 19:45:53 -0500

    Fix hotkey in grub menu.

---
+++ config/includes.chroot/root/etc/firefox/user.js
@@ -0,0 +1,5 @@
+user_pref("browser.aboutwelcome.enabled", false);
+user_pref("browser.newtabpage.enabled", false);
+user_pref("browser.newtab.url", "about:blank");
+user_pref("browser.startup.homepage", "about:blank");
+user_pref("datareporting.policy.firstRunURL", "");
