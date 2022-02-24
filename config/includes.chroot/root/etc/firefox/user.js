764092d819f902aab5bda96af6ed7566f724c181
Juergen Fiedler <juergen@fiedlerfamily.net>
2022-02-23 19:47:24 -0500

    Make Firefox skip the welcome page.

--- config/includes.chroot/root/etc/firefox/user.js
+++ config/includes.chroot/root/etc/firefox/user.js
@@ -1,5 +1,6 @@
 user_pref("browser.aboutwelcome.enabled", false);
 user_pref("browser.newtabpage.enabled", false);
 user_pref("browser.newtab.url", "about:blank");
-user_pref("browser.startup.homepage", "about:blank");
+user_pref("browser.newtabpage.activity-stream.feeds.section.topstories", false);
+user_pref("browser.startup.homepage", "about:home");
 user_pref("datareporting.policy.firstRunURL", "");
